15f7acb6f924acf87a781143ebef4f33aa7dc4d8
ice <ice-bt@hotmail.com>
2024-01-25 22:55:58 +0100

    one

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final int IMPLICITLY_WAIT_TIME=10;
 	
-	public static final int PAGE_LOAD_TIME =5;
+	public static final int PAGE_LOAD_TIME =15;
 	
 	public static final int EXPLICIT_WAIT_BASIC_TIME=15;
 	
